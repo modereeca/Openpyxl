c50df1443969779a0bcf5bea5f26a29d736ac6d9
mkadirtan <muhammedkadirtan@gmail.com>
2021-01-20 03:16:09 +0300

    Barkodlar doğru bir şekilde siliniyor

--- barkod.py
+++ barkod.py
@@ -1,41 +1,34 @@
 # 'Barkod' ve 'İtem No' lu kısımların açıklama sütunundan silinmesi.
 
-"""
-Barkodu: 8681689375997<br />
-Item No : LOL7599<br />
-"""
-
-
 def barkod(ws, max_row):
-    for satir in range(2, max_row+1):
+    for satir in range(2, max_row + 1):
         aciklama_sutun = 8
         a = str(ws.cell(satir, aciklama_sutun).value)
-
-        if a.find("Barkodu") != -1:
-            ilk_index = a.find("Barkodu")
-            son_index = -1
-            for test_index in range(1, 35):
-                if (ilk_index + test_index) >= len(a):
-                    break
-                character = a[ilk_index + test_index]
-                if character == ">":
-                    son_index = ilk_index + test_index
-                    break
-            if son_index != -1:
-                b = a.replace(a[ilk_index:son_index], "")
-                ws.cell(satir, aciklama_sutun, value=b)
-
-        elif a.find("BARKOD") != -1:
-            ilk_index = a.find("BARKOD")
-            son_index = -1
-            for test_index in range(1, 35):
-                if (ilk_index + test_index) >= len(a):
-                    break
-                character = a[ilk_index + test_index]
-                if character == ">":
-                    son_index = ilk_index + test_index
-                    break
-            if son_index != -1:
-                b = a.replace(a[ilk_index:son_index], "")
-                ws.cell(satir, aciklama_sutun, value=b)
+        a = clear_from_text_with_br(a, "Barkodu:")
+        a = clear_from_text_with_br(a, "Barkodu :")
+        a = clear_from_text_with_br(a, "BARKOD")
+        a = clear_from_text_with_br(a, "Item No")
+        a = clear_from_text_with_br(a, "İtem No")
+        a = clear_from_text_with_br(a, "ITEM NO")
+        ws.cell(satir, aciklama_sutun, value=a)
     return ws
+
+
+def clear_from_text_with_br(text, subtext):
+    ilk_index = text.find(subtext)
+
+    if ilk_index == -1:
+        return text
+
+    son_index = -1
+    for test_index in range(ilk_index, len(text) - 7):
+        br_etiketi = text[test_index:test_index + 1]
+        if br_etiketi == "<":
+            son_index = test_index + 1
+            break
+
+    if son_index == -1:
+        return text
+
+    new_text = text.replace(text[ilk_index:son_index], "")
+    return new_text
