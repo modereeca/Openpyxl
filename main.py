a9780f44a4524d26e525093d569f70b5d03b1f5d
mkadirtan <muhammedkadirtan@gmail.com>
2021-01-20 03:16:20 +0300

    Yorum satırı kaldırıldı

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from varyasyon import varyasyon
 from breadcrumb import breadcrumb
 from barkod import barkod
 
-# ws = varyasyon(ws, max_row)
+ws = varyasyon(ws, max_row)
 print("Varyasyon tanımlandı")
 ws = breadcrumb(ws, max_row)
 print("Diğer> işareti değiştirildi")
-# ws = barkod(ws, max_row)
+ws = barkod(ws, max_row)
 print("Barkod ve Item No satırları silindi")
 
 print("Kaydediliyor...")
