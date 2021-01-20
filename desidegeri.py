c39eeb14a92b750da17bf14908346d1e77e106c6
modereeca <45945202+modereeca@users.noreply.github.com>
2021-01-20 03:52:52 +0300

    Desi değerlerini kargo ağırlığına tanımlayan fonksiyon oluşturuldu.

--- desidegeri.py
+++ desidegeri.py
@@ -1,4 +1,14 @@
 # Desi değerleri
 
-def desi_degeri():
-    pass
+def desi_degeri(ws, max_row):
+    for satir in range(2, max_row + 1):
+        aciklama_sutun = 8
+        kargoagirligi_sutun = 59
+        a = str(ws.cell(satir, aciklama_sutun).value)
+        str(ws.cell(satir, kargoagirligi_sutun, value=""))
+        if "Desi Değeri:" in a:
+            b = a.find("Desi Değeri")
+            c = a[b:len(a)]
+            str(ws.cell(satir, aciklama_sutun, value=a.replace(c, "")))
+            str(ws.cell(satir, kargoagirligi_sutun, value=c))
+    return ws
--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from excel import wb, ws, max_row
 from varyasyon import varyasyon
 from breadcrumb import breadcrumb
 from barkod import barkod
+from desidegeri import desi_degeri
 
-ws = varyasyon(ws, max_row)
+# ws = varyasyon(ws, max_row)
 print("Varyasyon tanımlandı")
-ws = breadcrumb(ws, max_row)
+# ws = breadcrumb(ws, max_row)
 print("Diğer> işareti değiştirildi")
-ws = barkod(ws, max_row)
+# ws = barkod(ws, max_row)
 print("Barkod ve Item No satırları silindi")
+ws = desi_degeri(ws, max_row)
+print("desi değeri bulundu")
 
 print("Kaydediliyor...")
 wb.save(path.join(path.curdir, 'output.xlsx'))
