2c30739f159b5367d7a2b52b6479a6ad127d3dac
mkadirtan <muhammedkadirtan@gmail.com>
2021-01-20 11:32:14 +0300

    Desi değeri yeniden düzenlendi

--- desidegeri.py
+++ desidegeri.py
@@ -4,11 +4,19 @@ def desi_degeri(ws, max_row):
     for satir in range(2, max_row + 1):
         aciklama_sutun = 8
         kargoagirligi_sutun = 59
-        a = str(ws.cell(satir, aciklama_sutun).value)
-        str(ws.cell(satir, kargoagirligi_sutun, value=""))
-        if "Desi Değeri:" in a:
-            b = a.find("Desi Değeri")
-            c = a[b:len(a)]
-            str(ws.cell(satir, aciklama_sutun, value=a.replace(c, "")))
-            str(ws.cell(satir, kargoagirligi_sutun, value=c))
+
+        a = ws.cell(satir, aciklama_sutun).value
+
+        if a is None:
+            break
+
+        a = str(a)
+
+        if a.find("Desi Değeri") == -1:
+            break
+
+        index = a.find("Desi Değeri")
+        c = a[index:len(a)]
+        ws.cell(satir, aciklama_sutun, value=a.replace(c, ""))
+        ws.cell(satir, kargoagirligi_sutun, value=c)
     return ws
